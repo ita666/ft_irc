932ea9e2307a228bbc43d1702f4552d51ecc8d56
nugahh <nugah@DESKTOP-96KGKIL.localdomain>
2023-08-01 18:06:52 +0200

    Base multi-client

---
+++ srcs/multi.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+	// Create a socket
+	int listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == -1) {
+		cerr << "Can't create a socket!";
+		return -1;
+	}
+
+	// Bind thesockt to a IP / port
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);				   // host to network short
+	inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // convert IP address from text to binary form 127.0.0.1
+
+	if (bind(listening, (sockaddr *)&hint, sizeof(hint)) == -1) {
+		cerr << "Can't bind to IP/port";
+		return -2;
+	}
+
+	// Mark the socket for listening in
+	if (listen(listening, SOMAXCONN) == -1) {
+		cerr << "Can't listen!";
+		return -3;
+	}
+
+	fd_set master;
+
+	FD_ZERO(&master); // Initilize the master set to 0
+	FD_SET(listening, &master);
+
+	string password = "1234";
+	while (true) {
+		fd_set copy = master;
+		cout << "test" << endl;
+		if (select(FD_SETSIZE, &copy, nullptr, nullptr, nullptr) < 0) {
+			perror("select error");
+			exit(EXIT_FAILURE);
+		}
+		cout << "test" << endl;
+		for (int i = 0; i < FD_SETSIZE; i++) {
+			cout << "test" << endl;
+			if (FD_ISSET(i, &copy)) {
+				if (i == listening) {
+				cout << "CACA" << endl;
+					// this is a new connection
+					char password_request[] = "Enter password: ";
+					send(i, password_request, sizeof(password_request), 0);
+					int client_socket = accept(listening, nullptr, nullptr);
+					FD_SET(client_socket, &master);
+				} else {
+					char client_password[256];
+					if (recv(i, client_password, sizeof(client_password), 0) <= 0) {
+						perror("recv error");
+					}
+					else {
+						if (password != client_password){
+							// do whatever we do with connections
+							FD_CLR(i, &master);
+						}
+					}
+				}
+			}
+		}
+	return 0;
+	}
+}
