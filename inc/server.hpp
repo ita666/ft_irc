4c79d74d3f7696b670a33e5335d213770bcad3a2
Nugah <fwong@paul-f3Ar6s8.clusters.42paris.fr>
2023-08-16 17:22:03 +0200

    need to finish invite, join modified

--- inc/server.hpp
+++ inc/server.hpp
@@ -39,6 +39,7 @@ class Server: public Command{
     	void	User(int socket, vector<string>& arg, Client);
 
 		void	checkFlag(int socket, vector<string>& arg, int i, Client);
+		bool	checkChannelName(string name);
 
 	private:
 		int							_port;
--- srcs/commands/Invite.cpp
+++ srcs/commands/Invite.cpp
@@ -9,9 +9,13 @@ void Server::Invite(int socket, vector<string>& arg, Client cl) {
 	string guest = arg[0];
 	string channelName = arg[1];
 
+	if (_clients[socket].checkRight() == false)
+		return _clients[socket].sendMessage(ERR_CHANOPRIVSNEEDED(channelName, cl.getNickname()));
+	if (arg.size() < 2)
+		return _clients[socket].sendMessage(ERR_NEEDMOREPARAMS(cl.getNickname(), "INVITE"));
 	if (_channels[channelName].findInvited(guest) != guest) {
 		_channels[channelName].addGuest(guest);
 	}
 	else
-		_clients[socket].sendMessage(ERR_ALREADYREGISTRED(guest));
+		_clients[socket].sendMessage(ERR_USERONCHANNEL(cl.getNickname(), channelName, guest));
 }
