1aa2de870f0ad70f09b0ce1725895bfc5753eaef
yanthoma <yanthoma@student.42.fr>
2023-08-18 13:01:43 +0200

    42

--- srcs/commands/Kick.cpp
+++ srcs/commands/Kick.cpp
@@ -12,10 +12,19 @@ void 	Server::Kick(int socket, vector<string>& arg, Client cl){
 	string comment = " bad boy !";
 	Client guestCl = _stringToClients[guest];
 
+
+	cout << arg.size() << "\n";
+	cout << "guest " << arg[1] << "\n";
+	cout  << "channelname " << arg[0] << "\n";
+	cout << "current client nick " << currentClientNickname << "\n";
+	cout << "current client user " << currentClientUsername << "\n";
 	if (_clients[socket].checkRight() == false) // check if the user is an operator
+	{
+
 		return _clients[socket].sendMessage(ERR_NOPRIVILEGES(currentClientNickname));
+	}
 	if (arg.size() < 2) // check if there is enough arguments
-		return _clients[socket].sendMessage(ERR_NEEDMOREPARAMS(currentClientNickname, "KICK"));
+		{return _clients[socket].sendMessage(ERR_NEEDMOREPARAMS(currentClientNickname, "/KICK"));}
 	if (checkChannelName(channelName) == false) // check if the channel exists
 		return _clients[socket].sendMessage(ERR_NOSUCHCHANNEL(currentClientNickname, channelName));
 	if (_channels[channelName].isUserInChannel(currentClientNickname) == false) // check if the user is in the channel, if not return an error
@@ -23,9 +32,9 @@ void 	Server::Kick(int socket, vector<string>& arg, Client cl){
 	if (_channels[channelName].isUserInChannel(guest) == false) // check if the guest is in the channel, if yes return an error
 		return _clients[socket].sendMessage(ERR_USERNOTINCHANNEL(currentClientNickname, channelName, guest));
 	//if (_channels[channelName].findInvited(guest) != guest) { // check if the guest is already invited, if not add him to the invited list
-	if (!arg[2].empty()) { comment = arg[2]; }
-	_channels[channelName].addGuest(guest);
-		//_clients[socket].sendMessage(RPL_INVITING(currentClientNickname, guest, channelName));
+	if (arg[2].length() ==  1) { comment = arg[2]; }
+	//_channels[channelName].addGuest(guest);
+	_clients[socket].sendMessage(RPL_INVITING(currentClientNickname, guest, channelName));
 	guestCl.sendMessage(KICK(currentClientNickname, _clients[socket].getUser(), channelName, guest, comment));
 }
 
